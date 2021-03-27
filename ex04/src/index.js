4afc33a7aff4c4eaa774b8a50554a32eddefc32d
HM02fly <46242213+HM02fly@users.noreply.github.com>
2021-03-27 04:19:39 +0100

    (Y)

---
+++ ex04/src/index.js
@@ -0,0 +1,19 @@
+// src/index.js
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends Component {
+  // Change this line
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
